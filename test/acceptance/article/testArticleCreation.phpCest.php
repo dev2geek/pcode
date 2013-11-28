d83e51bccd50412464ba5d70c0a2d206443aae9f
dev2geek <dev2geek@163.com>
2013-11-28 23:34:53 +0800

    cest

---
+++ test/acceptance/article/testArticleCreation.phpCest.php
@@ -0,0 +1,21 @@
+<?php
+namespace \article;
+use \WebGuy;
+
+class testArticleCreationCest
+{
+
+    public function _before()
+    {
+    }
+
+    public function _after()
+    {
+    }
+
+    // tests
+    public function tryToTest(WebGuy $I) {
+    
+    }
+
+}
